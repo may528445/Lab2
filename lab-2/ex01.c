7942bd41e3bd5bc48115460a40d07247ad3ecefa
may528445 <68011381@kmitl.ac.th>
2025-07-26 11:43:17 +0000

    h

---
+++ lab-2/ex01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("May Thazin Hlaing\n");
+    printf("Tin Htun Aung\n");
+}
---
+++ lab-2/ex02.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Tin Htun Aung\n");
+    printf("Tin Htun Aung\n");
+}
